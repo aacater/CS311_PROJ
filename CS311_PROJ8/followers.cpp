2988c455a968d43e68372241eade6b951a4e6caa
aacater <aacater@alaska.edu>
2019-12-02 20:04:07 -0900

    added comments, added pause

--- CS311_PROJ8/followers.cpp
+++ CS311_PROJ8/followers.cpp
@@ -1,13 +1,23 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<vector>
-#include<algorithm>
-#include<map>
+// followers.h
+// Alex Cater, Kurt Nunn, Christopher Seamount
+// 2019-12-03
+//
+// For CS 311 Fall 2019
+// Source File for Project 8, Exercise A
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <sstream>
 
 typedef std::vector<std::string> ValType;
 typedef std::map<std::string, ValType> MapType;
 
+// prints stats about list
+//		number of distinct words and key + values
 void printList(const MapType& list)
 {
 	std::cout << "Number of distinct words: " << list.size() << std::endl << std::endl;
@@ -22,6 +32,7 @@ void printList(const MapType& list)
 	}
 }
 
+// sorts values for each key
 void sortList(MapType& list)
 {
 	for (auto &n : list)
@@ -30,6 +41,7 @@ void sortList(MapType& list)
 	}
 }
 
+// inserts a word into list based on the word it followed
 void insertWord(MapType& list, const std::string& currentword, std::string& previousword)
 {
 	if (list.find(previousword) == list.end())
@@ -49,6 +61,7 @@ void insertWord(MapType& list, const std::string& currentword, std::string& prev
 	return;
 }
 
+// read a file and properly create list
 MapType makeList(std::ifstream& myfile)
 {
 	MapType list;
@@ -56,8 +69,12 @@ MapType makeList(std::ifstream& myfile)
 	std::string previousword = "";
 
 	myfile >> previousword;
-	while (!myfile.eof())
+	while (myfile)
 	{
+		if (myfile.eof())
+		{
+			break;
+		}
 		myfile >> currentword;
 		insertWord(list, currentword, previousword);
 	}
@@ -68,17 +85,28 @@ MapType makeList(std::ifstream& myfile)
 	return list;
 }
 
+// userPause
+// Wait for user to press ENTER: read all chars through first newline.
+void userPause()
+{
+	std::cout.flush();
+	while (std::cin.get() != '\n');
+}
+
+// Main of file
+// opens and creates list of followers from contents
 int main()
 {
 	std::string filename;
 	std::cout << "Enter a filename: ";
 	std::getline(std::cin, filename);
-	
 	std::ifstream myfile;
 	myfile.open(filename);
 	if (!myfile)
 	{
 		std::cout << "Unable to open file \"" << filename << "\"" << std::endl;
+		std::cout << "Press ENTER to quit ";
+		userPause();
 		return -1;
 	}
 
@@ -87,6 +115,10 @@ int main()
 	printList(list);
 
 	myfile.close();
+
+	// Wait for user
+	std::cout << "Press ENTER to quit ";
+	userPause();
 
 	return 0;
 }
